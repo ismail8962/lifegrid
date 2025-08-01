3494b94083265bb1ab15eda9e18cff2c52f2743c
ismail8962 <ismailshaikh0398447@gmail.com>
2025-08-01 12:39:01 +0530

    Initial commit: Uploaded LifeGrid full source

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>🌐 Welcome to LifeGrid</h1>
+      <p>The future of your digital life starts here.</p>
+    </div>
+  );
+}
